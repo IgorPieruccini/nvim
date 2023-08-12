4eaeb29a7e9873cabf323043a3b4fbf6405d5b61
igorpieruccini <igorpieruccini@gmail.com>
2023-08-12 17:15:14 +0200

    Add NeoTreeReveal shortcut

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("n", "<C-b><C-f>", "<cmd>Neotree float git_status<CR>")
 
 vim.keymap.set("n", "<C-b>", "<cmd>NeoTreeFocusToggle<CR>")
+
+vim.keymap.set("n", "<C-r>", "<cmd>NeoTreeReveal<CR>")
